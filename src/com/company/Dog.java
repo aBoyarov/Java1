ab62ea3c2aeae5334fc022aead1e96a257b0dda6
Andrey Boyarov <Boyarob1995@yandex.ru>
2022-03-25 22:17:43 +0300

    Урок 6

--- src/com/company/Dog.java
+++ src/com/company/Dog.java
@@ -1,1 +1,33 @@
 package com.company;
+
+public class Dog extends Animal {
+    public static int countDog = 0;
+
+    @Override
+    public void run(int length) {
+        if (length <= 500 && length > 0) {
+            System.out.printf("Хороший мальчик %s  пробежал %d метров \n", getName(), length);
+        } else if (length < 0) {
+            System.out.printf("%s хочет побегать \n", getName());
+        } else System.out.printf("%s не сможет убежать так далеко \n", getName());
+    }
+
+    @Override
+    public void toSwim(int length) {
+        if (length <= 10 && length > 0) {
+            System.out.printf("%s проплыл %d метров \n", getName(), length);
+
+        } else if (length < 0) {
+            System.out.println("Где ВОДА?");
+        } else System.out.printf("%s не сможет уплыть так далеко \n", getName());
+    }
+
+    public Dog(String name, int age, String color) {
+        super(name, age, color);
+        countDog++;
+    }
+
+    public static int getCount() {
+        return countDog;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,1 +1,16 @@
 package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog("Шарик", 4, "чёрный");
+        Animal cat = new Cat("Пиксель", 2, "пятнистый");
+
+
+        cat.run(120);
+        cat.toSwim(1);
+        dog.run(420);
+        dog.toSwim(5);
+        Animal.count();
+
+    }
+}
